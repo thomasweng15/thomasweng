5b964a5623349008c6e1c9b3a27f8512c7f94f6a
Thomas Weng <thomas.weng11@gmail.com>
2015-06-01 15:30:11 -0400

    Used ejs and partials for views

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+var ejs = require('ejs');
 var express = require('express');
+var partials = require('express-partials');
 
-var server = express();
-server.use(express.static(__dirname));
+var app = express();
+
+app.use(partials());
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// include public directory for static assets
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res, next) {
+	res.render('index');
+});
 
 var port = 3000;
-server.listen(port, function() {
+app.listen(port, function() {
 	console.log('server listening on port ' + port);
 });
