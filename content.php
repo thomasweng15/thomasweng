867e5c194f3c1bc8ace9197cb35d089d3d219301
Thomas Weng <thomas.weng11@gmail.com>
2015-06-01 00:32:32 -0400

    Included bootstrap css and added content

--- content.php
+++ content.php
@@ -1,1 +1,50 @@
-<h1>Hello World!</h1>
+<div class="container-fluid">
+  <div class="text-center">
+    <h1>Hi, I'm Thomas.</h1>
+    <h2 class="lead">This is my website.
+  </div>
+
+  <div id="about" class="section">
+    <h2>About Me</h2>
+    <div class="blurb features">
+      <p>
+        </span class="glyphicon glyphicon-home">
+        I hail from New York City.
+      </p>
+      <p>
+        </span class="glyphicon glyphicon-education">
+        I attended Yale University, where I received a B.S. in Computer Science and a B.A. in Economics.
+      </p>
+      <p>
+        </span class="glyphicon glyphicon-briefcase">
+        I will start working as a Software Development Engineer at Microsoft in the fall.
+      </p>
+    </div>
+  </div>
+
+  <div id="experience" class="section">
+    <h2>Experience</h2>
+    <div class="blurb row">
+      <div class="col-md-4">
+        <h3>Research</h3>
+        <p>Research experience with the Yale Social Robotics Lab</p>
+      </div>
+      <div class="col-md-4">
+        <h3>Work</h3>
+        <p>Work experience</p>
+      </div>
+      <div class="col-md-4">
+        <h3>Projects</h3>
+        <p>Personal Projects</p>
+      </div>
+    </div>
+  </div>
+
+  <div id="extra" class="section">
+    <h2>Extra</h2>
+    <div class="blurb">
+      <h3>Blog</h3>
+      <p>Check out my blog.</p>
+    </div>
+  </div>
+</div>
--- layout.php
+++ layout.php
@@ -2,6 +2,8 @@
 <html>
   <head>
     <title>Thomas Weng</title>
+    <link rel="stylesheet" href="public/stylesheets/bootstrap.css">
+    <link rel="stylesheet" href="public/stylesheets/style.css">
   </head>
   <body>
     <div id="content">
