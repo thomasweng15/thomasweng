e705b2883c57adefcf1f67586dbd5e5d63af302a
Thomas Weng <thomas.weng11@gmail.com>
2015-06-08 00:51:09 -0400

    Added favicon

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var ejs = require('ejs');
 var express = require('express');
 var partials = require('express-partials');
+var favicon = require('serve-favicon')
 
 var app = express();
 
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs');
 
 // include public directory for static assets
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get('/', function (req, res, next) {
 	res.render('index');
