2e89449e0782f056aaf355dee8e655307177b3c7
Thomas Weng <thomas.weng11@gmail.com>
2015-06-11 14:26:44 -0400

    Add smooth scroll and increased opacity of grad jpg

---
+++ public/js/animations.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  smoothScroll.init({
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInQuad', // Easing pattern to use
+    updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+  });
+
+  $("#name").click(function () {
+
+  });
+});
