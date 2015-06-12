a5b062f7cb3f5793d11d4ee9996ca30d00b9764c
Thomas Weng <thomas.weng11@gmail.com>
2015-06-12 14:07:59 -0400

    Fix overflow problem and designed mobile menu

--- public/js/animations.js
+++ public/js/animations.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
   smoothScroll.init({
     speed: 500, // Integer. How fast to complete the scroll in milliseconds
     easing: 'easeInQuad', // Easing pattern to use
-    updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+    updateURL: true // Boolean. Whether or not to update the URL with the anchor hash on scroll
   });
 });
